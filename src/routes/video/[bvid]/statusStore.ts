7d765e26b8412dcab59c52491e0dc8d457b9791d
Chatnoir Miki <cmiki@amono.me>
2023-06-09 07:29:03 +0800

    Add status store and type

---
+++ src/routes/video/[bvid]/statusStore.ts
@@ -0,0 +1,10 @@
+import { derived, writable } from 'svelte/store';
+import type { Status } from '../../../types/StatusType';
+
+export const status = writable<Status>({
+	statusMessage: '加载中...',
+	progress: 0
+});
+
+export const statusMessage = derived(status, ($status) => $status.statusMessage);
+export const progress = derived(status, ($status) => $status.progress);
---
+++ src/types/StatusType.ts
@@ -0,0 +1,4 @@
+export type Status = {
+	statusMessage: '加载中...' | '测试';
+	progress: number;
+};
