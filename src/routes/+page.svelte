929891dba3b0fc038789780d7d3700605b773507
Chatnoir Miki <cmiki@amono.me>
2023-06-09 06:55:15 +0800

    Add component html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import StatusWheel from './video/[bvid]/StatusWheel.svelte';
+</script>
+
+<div class="page relative h-full">
+	<div class="progress-bar absolute top-0 left-0" />
+	<main class="w-full h-full flex flex-row">
+		<section class="w-full h-full flex flex-col justify-center">
+			<div class="mx-auto flex flex-col gap-20 lg:ml-56">
+				<StatusWheel />
+				<p
+					class="px-4 lg:px-0 message-bar message-normal font-medium text-lg text-center lg:text-left tracking-wider transition-all duration-500"
+				>
+					请稍等片刻
+				</p>
+			</div>
+		</section>
+	</main>
+</div>
---
+++ src/routes/video/[bvid]/StatusWheel.svelte
@@ -0,0 +1,5 @@
+<article id="status-wheel" class="status-wheel-frame font-light tracking-wide overflow-hidden">
+	<div class="status-wheel transition-all duration-500 flex flex-col">
+		<p class="status-item">加载中...</p>
+	</div>
+</article>
