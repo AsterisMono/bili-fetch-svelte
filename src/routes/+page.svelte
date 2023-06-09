800ebca704e4ac72b5ba815806f9370cb6325914
Chatnoir Miki <cmiki@amono.me>
2023-06-10 06:59:21 +0800

    Fix: File name casing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import MessageBar from './video/[bvid]/MessageBar.svelte';
 	import StatusWheel from './video/[bvid]/StatusWheel.svelte';
-	import { infoMessage, status } from './video/[bvid]/StatusStore';
+	import { status, infoMessage } from './video/[bvid]/StatusStore';
 
 	let count = 1;
 </script>
