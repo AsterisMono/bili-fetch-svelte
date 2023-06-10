1ab71eb852c94a191e8bbb5e9e27cb27f4828979
Chatnoir Miki <cmiki@amono.me>
2023-06-10 15:18:09 +0800

    Fix: sm layout status wheel font size

--- src/lib/components/StatusWheel.svelte
+++ src/lib/components/StatusWheel.svelte
@@ -8,32 +8,31 @@
 	});
 
 	// Dynamic status text size with media query
+	// UI font size will be handled by CSS to avoid layout jump in SSR
+	// This text size is solely for translate value calculation
 	let statusTextSize = 4.5;
 
-	const onMediaChange = (e: MediaQueryListEvent) => {
-		if (e.matches) statusTextSize = 2.8;
-		else statusTextSize = 4.5;
+	const respondToMediaChange = () => {
+		statusTextSize = window.matchMedia('(max-width: 640px)').matches ? 2.8 : 4.5;
 	};
 
 	let mediaWatcher: MediaQueryList;
 	onMount(() => {
+		respondToMediaChange(); // Determine initial font size
 		mediaWatcher = window.matchMedia('(max-width: 640px)');
-		mediaWatcher.addEventListener('change', onMediaChange);
+		mediaWatcher.addEventListener('change', respondToMediaChange);
 	});
 
 	// Clear the media watcher on component unload
 	onDestroy(() => {
-		if (mediaWatcher) mediaWatcher.removeEventListener('change', onMediaChange); // TODO: Why this unloads first?
+		if (mediaWatcher) mediaWatcher.removeEventListener('change', respondToMediaChange); // TODO: Why this unloads first?
 	});
 
 	// Calculate status translateY
 	$: translateY = -2 * statusTextSize * (statusList.length - 1);
 </script>
 
-<article
-	class="status-wheel-frame font-light tracking-wide overflow-hidden"
-	style="--status-text-size: {statusTextSize}rem"
->
+<article class="status-wheel-frame font-light tracking-wide overflow-hidden">
 	<div
 		class="status-wheel transition-all duration-500 flex flex-col"
 		style="transform: translateY({translateY}rem);"
@@ -44,7 +43,16 @@
 	</div>
 </article>
 
-<style>
+<style lang="postcss">
+	:root {
+		--status-text-size: 2.8rem;
+	}
+
+	@media screen(sm) {
+		:root {
+			--status-text-size: 4.5rem;
+		}
+	}
 	.status-wheel-frame {
 		height: var(--status-text-size);
 	}
