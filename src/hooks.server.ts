47065e8d6d30127518f06af94f6e246fafcc2bac
Chatnoir Miki <cmiki@amono.me>
2023-11-08 21:18:31 +0800

    use vite meta env

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,6 @@ import type { HandleServerError } from '@sveltejs/kit';
 export const handleError = (async ({ error, event }) => {
 	// Send error analytics
 	return {
-		message: import.meta.env.DEV ? '喔唷，出错啦！' : `${JSON.stringify(error)}`
+		message: import.meta.env.VITE_CF_PAGES_DEV ? `${JSON.stringify(error)}` : '喔唷，出错啦！'
 	};
 }) satisfies HandleServerError;
