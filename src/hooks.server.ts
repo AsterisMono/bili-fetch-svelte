3f2f1333384702d471af9bd315cd35898084ed82
Chatnoir Miki <cmiki@amono.me>
2023-11-08 21:21:09 +0800

    Debug settings going production (#5)
    * debug message
    
    * try to fix redirect issue
    
    * Revert "debug message"
    
    This reverts commit 73ebc942883af9b1486562fa581bee0b90b5b5f9.
    
    * Revert "try to fix redirect issue"
    
    This reverts commit d5e84ebe7d16aa7087fbc3e86ec1327517d482c7.
    
    * error logging
    
    * modify error logging var switch
    
    * use vite meta env

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,6 @@ import type { HandleServerError } from '@sveltejs/kit';
 export const handleError = (async ({ error, event }) => {
 	// Send error analytics
 	return {
-		message: '喔唷，出错啦！'
+		message: import.meta.env.VITE_CF_PAGES_DEV ? `${JSON.stringify(error)}` : '喔唷，出错啦！'
 	};
 }) satisfies HandleServerError;
