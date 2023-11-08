fbb93f6766f4ce0efe602712c56a04c7da700000
Chatnoir Miki <cmiki@amono.me>
2023-11-08 20:59:27 +0800

    error logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,9 @@ import type { HandleServerError } from '@sveltejs/kit';
 export const handleError = (async ({ error, event }) => {
 	// Send error analytics
 	return {
-		message: '喔唷，出错啦！'
+		message:
+			import.meta.env.CF_PAGES_BRANCH === 'production'
+				? '喔唷，出错啦！'
+				: `${import.meta.env.CF_PAGES_BRANCH}: ${(error as Error).message}`
 	};
 }) satisfies HandleServerError;
