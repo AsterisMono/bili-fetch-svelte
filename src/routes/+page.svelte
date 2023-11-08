ecce5ec45dd0f74b3f6fc158dd9831f4659477d7
Chatnoir Miki <cmiki@amono.me>
2023-11-08 20:32:19 +0800

    Implement clipboard detection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,91 @@
-<script>
+<script lang="ts">
+	import { BvidRegex } from '$lib/utils/Regex';
 	import { status, message, updateStatus, updateMessage } from '$lib/utils/StatusStore';
 	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { getRandomLoadingPrompt } from '$lib/utils/loadingPrompts';
+
+	let clipboardDetectLoopTimer: ReturnType<typeof setInterval>;
+
+	async function requireCopyLink() {
+		await updateMessage('复制一个B站链接，然后回到本页来开始下载', 2000);
+	}
+
+	async function requireManualInput() {
+		await updateMessage('在地址栏后输入BV号来开始下载', 2000);
+	}
+
+	async function requireUserClick() {
+		await updateMessage('点击这里识别你复制的链接', 2000, async () => await parseClipboard());
+	}
+
+	async function spicyFace() {
+		await updateStatus('(`･д･)');
+	}
+
+	async function parseClipboard() {
+		let clipboard: string | undefined = undefined;
+		try {
+			clipboard = await navigator.clipboard.readText();
+		} catch (e) {
+			const errorMessage = (e as { message: string }).message;
+			if (errorMessage.includes('denied')) {
+				await requireCopyLink();
+				return;
+			} else if (errorMessage.includes('focus')) {
+				// 需要用户手动点击页面才能读取剪贴板
+				await requireUserClick();
+				return;
+			} else {
+				spicyFace();
+				await updateMessage('剪贴板读取失败', 3000);
+				await requireCopyLink();
+				return;
+			}
+		}
+		await updateMessage(getRandomLoadingPrompt(), 1500);
+		const bvid = clipboard?.match(BvidRegex);
+		if (bvid) {
+			goto(`/${bvid[0]}`);
+		} else {
+			spicyFace();
+			await updateMessage('没有在剪贴板中发现BV号', 2000);
+			await requireCopyLink();
+			clipboardDetectLoopTimer = setInterval(clipboardDetectLoop, 1000);
+		}
+	}
+
+	async function clipboardDetectLoop() {
+		try {
+			if (document.hidden) return;
+			const clipboard = await navigator.clipboard.readText();
+			const bvid = clipboard?.match(BvidRegex);
+			if (bvid) {
+				clearInterval(clipboardDetectLoopTimer);
+				goto(`/${bvid[0]}`);
+			}
+		} catch (e) {
+			const errorMessage = (e as { message: string }).message;
+			if (errorMessage.includes('focus')) {
+				await updateMessage('点击这里继续', 1500, requireCopyLink);
+				return;
+			}
+		}
+	}
 
 	// Here comes the show
 	onMount(async () => {
 		updateStatus('(`･∀･)');
-		await updateMessage('你好呀！', 2000);
-		await updateMessage('在网址后输入BV号来开始下载', 2000);
+		// @ts-ignore
+		const { state: clipboardState } = await navigator.permissions.query({ name: 'clipboard-read' });
+		if (clipboardState === 'granted') {
+			parseClipboard();
+		} else if (clipboardState === 'prompt') {
+			await updateMessage('你好呀！', 2000);
+			await requireUserClick();
+		} else if (clipboardState === 'denied') {
+			await requireManualInput();
+		}
 	});
 </script>
 
--- src/routes/[bvid=bvid]/+page.svelte
+++ src/routes/[bvid=bvid]/+page.svelte
@@ -14,103 +14,110 @@
 		else title = `✔ ${data.videoTitle}`;
 	}
 	progress.subscribe(reactiveTitle);
-	onMount(async () => {
-		try {
-			// 1. Load FFMpeg
-			const ffmpeg = await loadFFmpeg();
-			const { bvid, cid, coverImageUrl, videoTitle, videoUploader, videoUrl } = data;
-
-			// stage: raw(mp4), converted(mp3), bundled(mp3)
-			const getFileName = (stage: string, ext: string) => `${bvid}_${cid}_${stage}.${ext}`;
-
-			// 2. Download video
-			updateProgress(0.1);
-			updateMessage(`${videoTitle} - ${videoUploader}`);
-			await updateStatus('正在下载视频');
-
+	onMount(() =>
+		setTimeout(async () => {
 			try {
-				ffmpeg.FS(
-					'writeFile',
-					getFileName('raw', 'mp4'),
-					await fetchFile(videoUrl, (ratio) => updateProgress(0.1 + ratio * 0.4))
-				);
+				// 1. Load FFMpeg
+				const ffmpeg = await loadFFmpeg();
+				const { bvid, cid, coverImageUrl, videoTitle, videoUploader, videoUrl } = data;
+
+				// stage: raw(mp4), converted(mp3), bundled(mp3)
+				const getFileName = (stage: string, ext: string) => `${bvid}_${cid}_${stage}.${ext}`;
+
+				// 2. Download video
+				updateProgress(0.1);
+				updateMessage(`${videoTitle} - ${videoUploader}`);
+				await updateStatus('正在下载视频');
+
+				try {
+					ffmpeg.FS(
+						'writeFile',
+						getFileName('raw', 'mp4'),
+						await fetchFile(videoUrl, (ratio) => updateProgress(0.1 + ratio * 0.4))
+					);
+				} catch (e) {
+					throw Error('视频下载失败');
+				}
+
+				// 3. convert video to audio
+				updateProgress(0.5);
+				await updateStatus('正在进行转换');
+				ffmpeg.setProgress(({ ratio }) => updateProgress(0.5 + ratio * 0.3));
+				try {
+					await ffmpeg.run(
+						'-i',
+						getFileName('raw', 'mp4'),
+						'-q:a',
+						'0',
+						'-map',
+						'a',
+						getFileName('converted', 'mp3')
+					);
+				} catch (e) {
+					throw Error('转换失败');
+				}
+				ffmpeg.setProgress(() => undefined);
+
+				// 4. bundle metadata
+				updateProgress(0.8);
+				await updateStatus('正在给文件附魔');
+				try {
+					ffmpeg.FS(
+						'writeFile',
+						getFileName('albumart', 'jpg'),
+						await fetchFile(coverImageUrl, (ratio) => updateProgress(0.8 + ratio * 0.1))
+					);
+
+					await ffmpeg.run(
+						'-i',
+						getFileName('converted', 'mp3'),
+						'-i',
+						getFileName('albumart', 'jpg'),
+						'-map',
+						'0:0',
+						'-map',
+						'1:0',
+						'-c',
+						'copy',
+						'-id3v2_version',
+						'3',
+						'-metadata:s:v',
+						`title="Album cover"`,
+						'-metadata:s:v',
+						`comment="Cover (Front)"`,
+						'-metadata',
+						`artist=${videoUploader}`,
+						'-metadata',
+						`title=${videoTitle}`,
+						'-metadata',
+						`album=${videoTitle}`,
+						getFileName('out', 'mp3')
+					);
+				} catch (e) {
+					throw Error('附加信息失败');
+				}
+
+				// 5. finish up
+				updateProgress(1.0);
+				await updateStatus('已完成');
+				offerFileAsDownload(ffmpeg, getFileName('out', 'mp3'), `${videoTitle}.mp3`);
+
+				await new Promise((resolve, reject) => setTimeout(resolve, 700));
+				updateMessage('喜欢吗？', 1000, () => {
+					updateMessage('谢谢。开发者已经收到你的👍。');
+				});
 			} catch (e) {
-				throw Error('视频下载失败');
+				if (
+					typeof e === 'object' &&
+					e !== null &&
+					'message' in e &&
+					typeof e.message === 'string'
+				) {
+					DisplayClientError(e.message);
+				}
 			}
-
-			// 3. convert video to audio
-			updateProgress(0.5);
-			await updateStatus('正在进行转换');
-			ffmpeg.setProgress(({ ratio }) => updateProgress(0.5 + ratio * 0.3));
-			try {
-				await ffmpeg.run(
-					'-i',
-					getFileName('raw', 'mp4'),
-					'-q:a',
-					'0',
-					'-map',
-					'a',
-					getFileName('converted', 'mp3')
-				);
-			} catch (e) {
-				throw Error('转换失败');
-			}
-			ffmpeg.setProgress(() => undefined);
-
-			// 4. bundle metadata
-			updateProgress(0.8);
-			await updateStatus('正在给文件附魔');
-			try {
-				ffmpeg.FS(
-					'writeFile',
-					getFileName('albumart', 'jpg'),
-					await fetchFile(coverImageUrl, (ratio) => updateProgress(0.8 + ratio * 0.1))
-				);
-
-				await ffmpeg.run(
-					'-i',
-					getFileName('converted', 'mp3'),
-					'-i',
-					getFileName('albumart', 'jpg'),
-					'-map',
-					'0:0',
-					'-map',
-					'1:0',
-					'-c',
-					'copy',
-					'-id3v2_version',
-					'3',
-					'-metadata:s:v',
-					`title="Album cover"`,
-					'-metadata:s:v',
-					`comment="Cover (Front)"`,
-					'-metadata',
-					`artist=${videoUploader}`,
-					'-metadata',
-					`title=${videoTitle}`,
-					'-metadata',
-					`album=${videoTitle}`,
-					getFileName('out', 'mp3')
-				);
-			} catch (e) {
-				throw Error('附加信息失败');
-			}
-
-			// 5. finish up
-			updateProgress(1.0);
-			await updateStatus('已完成');
-			offerFileAsDownload(ffmpeg, getFileName('out', 'mp3'), `${videoTitle}.mp3`);
-
-			await new Promise((resolve, reject) => setTimeout(resolve, 700));
-			updateMessage('喜欢吗？', 1000, () => {
-				updateMessage('谢谢。开发者已经收到你的👍。');
-			});
-		} catch (e) {
-			if (typeof e === 'object' && e !== null && 'message' in e && typeof e.message === 'string') {
-				DisplayClientError(e.message);
-			}
-		}
-	});
+		}, 100)
+	);
 </script>
 
 <svelte:head>
