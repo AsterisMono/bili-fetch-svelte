aa41b1c0e1199d011b68cc653e27e4fe3e367d7e
Chatnoir Miki <cmiki@amono.me>
2023-11-08 21:24:42 +0800

    Emergency maintenance mode

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,10 @@ import type { HandleServerError } from '@sveltejs/kit';
 export const handleError = (async ({ error, event }) => {
 	// Send error analytics
 	return {
-		message: import.meta.env.VITE_CF_PAGES_DEV ? `${JSON.stringify(error)}` : '喔唷，出错啦！'
+		message: import.meta.env.VITE_CF_PAGES_DEV
+			? `${JSON.stringify(error)}`
+			: import.meta.env.VITE_IN_MAINTENANCE
+			? '这个应用正处于维护模式，稍后再来吧'
+			: '喔唷，出错啦！'
 	};
 }) satisfies HandleServerError;
